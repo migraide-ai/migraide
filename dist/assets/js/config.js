2f522c9fafef8a497c670110c18c07fe9e61cd63
pocharis <arinze6@gmail.com>
2024-11-07 09:25:12 +0000

    adjusted nugge

--- dist/assets/js/config.js
+++ dist/assets/js/config.js
@@ -1,6 +1,6 @@
 // src/assets/js/config.js
-const API_BASE_URL = "https://beta.migraide.com/api/dashboard"; // Production URL
-// const API_BASE_URL = "http://127.0.0.1:5001/api/dashboard"; // Development URL
+// const API_BASE_URL = "https://beta.migraide.com/api/dashboard"; // Production URL
+const API_BASE_URL = "http://127.0.0.1:5001/api/dashboard"; // Development URL
 
 // Expose it globally so it can be accessed in any script
 window.API_BASE_URL = API_BASE_URL;
